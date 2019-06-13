2c57ca15f2bec715c51796b356d8675f6c1bb33a
Sammi Chekroud <schekrou@fmrib.ox.ac.uk>
2019-06-13 12:59:15 +0100

    push script to preprocess individual subjects data and output single file

--- analysis_scripts/r/wmConfidence_preprocessing.R
+++ analysis_scripts/r/wmConfidence_preprocessing.R
@@ -2,37 +2,48 @@
 library(tidyverse) # general data manipulation & plotting
 library(magrittr)  # allows use of more pipes
 
-wd <- '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence_eegfmri/data/datafiles/inside_pilot2'
+wd <- '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence'
 setwd(wd)
 
-#path to file with all subjects collated data
-fpath <- paste0(wd, '/wmConfidence_eegfmri_insidescanner_S02_allData.csv')
-
-df <- read.csv(fpath, header = T, as.is = T, sep = ',') %>% select(-X) # str(df) if you want to see the columns and values etc
-
-#mark trials where decision time (time to press space bar) outside 2.5 SDs of condition specific mean
-df %<>%
-  dplyr::mutate(cond = ifelse(cue ==0, 'neutral', 'cued')) %>%
-  dplyr::mutate(cond = as.factor(cond)) %>%
-  dplyr::mutate(cond = relevel(cond, 'neutral')) %>%
-  dplyr::mutate(DTcheck = 1) %>% #make new variable to approve discards
-  dplyr::group_by(subid, cue) %>%
-  dplyr::mutate(DTcheck = ifelse(DT <= ( mean(DT) + 2.5*sd(DT)) & DT >= (mean(DT) - 2.5*sd(DT)), 0, DTcheck)) %>%
-  as.data.frame()
+datapath <- paste0(dir, '/data/datafiles')
 
 
-library(circular)
-wrap <- function(x) (x+180)%%360 - 180 #function to wrap data between +/- 90 degrees. keeps sign of the response (-ve leftwards, +ve rightwards)
-wrap90 <- function(x) (x+90)%%180 - 90
-
-#bound response deviation between -90 and 90 because of symmetry
-df %<>% dplyr::mutate(rdif = wrap90(resp-targori))
-
-#compute some things about the confidence judgement
-df %<>%
-  dplyr::mutate(confwidth = abs(wrap90(confang-resp))) %>% #get width of confidence interval
-  dplyr::mutate(absrdif = abs(rdif)) %>% #get absolute deviation
-  dplyr::mutate(confdiff = abs(rdif) - confwidth) #get the difference between confidence width, and response deviation (i.e. whether target inside or outside confidence interval)
+# only doing this for specific subjects really, for now at least
+sublist <- seq(1, 2, by = 1)
 
 
-write.csv(df, file = paste0(wd, '/wmConfidence_eegfmri_insidescanner_S02_allData_preprocessed.csv'), eol = '\n', col.names = T) #save preprocessed data with new name
+for(sub in sublist){
+  subpath <- paste0(datapath, sprintf('/s%02d/', sub))
+  
+  fpath <- paste0(subpath, sprintf('wmConfidence_S%02d_allData.csv', sub))
+
+  df <- read.csv(fpath, header = T, as.is=T, sep = ',') %>% select(-X)
+  
+  df %<>%
+    dplyr::mutate(cond = ifelse(cue ==0, 'neutral', 'cued')) %>%
+    dplyr::mutate(cond = as.factor(cond)) %>%
+    dplyr::mutate(cond = relevel(cond, 'neutral')) %>%
+    dplyr::mutate(DTcheck = 1) %>% #make new variable to approve discards
+    dplyr::group_by(subid, cue) %>%
+    dplyr::mutate(DTcheck = ifelse(DT <= ( mean(DT) + 2.5*sd(DT)) & DT >= (mean(DT) - 2.5*sd(DT)), 0, DTcheck)) %>%
+    as.data.frame()
+  
+
+  library(circular)
+  wrap <- function(x) (x+180)%%360 - 180 #function to wrap data between +/- 90 degrees. keeps sign of the response (-ve leftwards, +ve rightwards)
+  wrap90 <- function(x) (x+90)%%180 - 90
+  
+  #bound response deviation between -90 and 90 because of symmetry
+  df %<>% dplyr::mutate(rdif = wrap90(resp-targori))
+  
+  #compute some things about the confidence judgement
+  df %<>%
+    dplyr::mutate(confwidth = abs(wrap90(confang-resp))) %>% #get width of confidence interval
+    dplyr::mutate(absrdif = abs(rdif)) %>% #get absolute deviation
+    dplyr::mutate(confdiff = abs(rdif) - confwidth) #get the difference between confidence width, and response deviation (i.e. whether target inside or outside confidence interval)
+  
+  detach("package:circular", unload=TRUE)
+  
+  fname <- paste0(subpath, sprintf('wmConfidence_S%02d_allData_preprocessed.csv', sub))
+  write.csv(df, file = fname, eol = '\n', col.names = T)
+}
