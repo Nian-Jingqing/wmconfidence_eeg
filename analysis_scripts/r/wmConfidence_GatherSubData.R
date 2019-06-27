69ae2346020bd6828ff56a776dd93b6684510466
Sammi Chekroud <schekrou@fmrib.ox.ac.uk>
2019-06-27 15:20:18 +0100

    gather sessions from each subject into one file per subject, then gather all subjects together into one master file

--- analysis_scripts/r/wmConfidence_GatherSubData.R
+++ analysis_scripts/r/wmConfidence_GatherSubData.R
@@ -2,24 +2,46 @@ library(dplyr)
 library(magrittr)
 getwd()
 dir <- '/home/sammirc/Desktop/DPhil/wmConfidence'
-#dir <- '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence'
+dir <- '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence'
 setwd(dir)
 
-datapath <- paste0(dir, '/data/datafiles')
+datapath <- paste0(dir, '/data/datafiles/preprocessed_data')
 
-# only doing this for specific subjects really, for now at least
-sublist <- seq(1, 2, by = 1)
+sublist <- seq(1, 7, by = 1)
+
+#this will collate all parts of the same subjects into one file per subject
 dataFiles = list(NULL)
-
 for(sub in sublist){
-  subpath <- paste0(datapath, sprintf('/s%02d/', sub))
-  
-  fpath <- paste0(subpath, sprintf('wmConfidence_S%02d_allData_preprocessed.csv', sub))
-  df <- read.csv(fpath, header = T, as.is = T, sep = ',') %>% select(-X)
+  if(sub <= 3){
+    fname <- paste0(datapath, sprintf('/wmConfidence_S%02d_allData_preprocessed.csv', sub))
+    df <- read.csv(fname, header = T, as.is = T, sep = ',') %>% select(-X)
+    outpath <- sprintf('%s/data/datafiles/s%02d/wmConfidence_S%02d_gathered_preprocessed.csv', dir, sub, sub)
+    write.csv(df, file = outpath, sep = ',', eol = '\n', dec = '.', col.names = T, row.names = F)
+  }
+  if(sub>3){ #only two parts to subjects after subject 3, so only need to concatenate two files from sub4 onwards
+    parts <- c('a', 'b')
+    fname1 <- paste0(datapath, sprintf('/wmConfidence_S%02d%s_allData_preprocessed.csv', sub, parts[1]))
+    fname2 <- paste0(datapath, sprintf('/wmConfidence_S%02d%s_allData_preprocessed.csv', sub, parts[2]))
+    
+    df1 <- read.csv(fname1, header = T, as.is = T, sep = ',') %>% select(-X)
+    df2 <- read.csv(fname2, header = T, as.is = T, sep = ',') %>% select(-X)
+
+    df <- df1 %>% dplyr::bind_rows(df2)
+    
+    outpath <- sprintf('%s/data/datafiles/s%02d/wmConfidence_S%02d_gathered_preprocessed.csv',dir, sub, sub)
+    
+    write.csv(df, file = outpath, sep = ',', eol = '\n', dec = '.', col.names = T, row.names = F)
+  }
+}
+
+#now collate all subjects and parts into one csv file
+dataFiles = list(NULL)
+for(sub in sublist){
+  fname <- sprintf('%s/data/datafiles/s%02d/wmConfidence_S%02d_gathered_preprocessed.csv',dir,sub,sub)
+  df <- read.csv(fname, header = T, as.is=T, sep = ',') #%>% select(-X)
   dataFiles[[sub]] <- df
-  
 }
 
 data <- do.call('rbind', dataFiles)
-fname <- paste0(dir, '/data/datafiles/wmConfidence_BehaviouralData_All.csv')
-write.csv(data, file=fname, sep = ',', eol = '\n', dec = '.', col.names=T, row.names = F)
+outname <- sprintf('%s/data/datafiles/wmConfidence_BehaviouralData_All.csv', dir)
+write.csv(data, file=outname, sep = ',', eol = '\n', dec = '.', col.names = T, row.names = F)
