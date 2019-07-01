554abe743a597450f61a2945367d5c8d25725887
Sammi Chekroud <sammi.r.chekroud@gmail.com>
2019-07-01 10:59:16 +0100

    update preprocessing scripts to work on incoming data, both sessions per participant

--- analysis_scripts/py/01_WMC_preprocess.py
+++ analysis_scripts/py/01_WMC_preprocess.py
@@ -5,8 +5,6 @@ Created on Tue Jun  4 12:03:07 2019
 
 @author: sammirc
 """
-
-
 import numpy as np
 import scipy as sp
 import pandas as pd
@@ -26,41 +24,68 @@ wd = '/home/sammirc/Desktop/DPhil/wmConfidence' #workstation wd
 os.chdir(wd)
 
 
-subs = np.array([1,2])
+subs = np.array([1,2, 3, 4, 5, 6, 7])
 
-
-subind = 2 #get first subject
-
-sub = dict(loc='workstation', id=subs[subind-1]) #get subject name for param extraction
-param = get_subject_info_wmConfidence(sub)
-
-#data for this expt collected using normal EEG caps, so just easycap-M1 layout used
-raw = mne.io.read_raw_eeglab(
-        input_fname = param['rawset'],
-        montage     = 'easycap-M1',
-        eog = ['VEOG', 'HEOG'],
-        preload = True
-        )
-raw.rename_channels({'PO6':'PO8'}) #error in the template set up file i made for mark, calls PO8 PO6 instead, so gest no location. correct this
-raw.set_montage('easycap-M1')
-
-raw.plot(duration = 4,
-         n_channels = len(raw.info['chs']),
-         color = dict(eog='blue', eeg = 'black'),
-         highpass = 1, lowpass = 40
-         )
-#clean raw data first
-raw.filter(1,40)
-
-ica = mne.preprocessing.ICA(n_components = .99, method = 'infomax').fit(raw)
-
-eog_epochs = mne.preprocessing.create_eog_epochs(raw)
-eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, threshold=2)
-ica.plot_scores(eog_scores)
-
-ica.plot_components(inst=raw)
-ica.exclude.extend(eog_inds)
-ica.apply(inst=raw)
-
-raw.save(fname = param['rawcleaned'], fmt='double') #save ica cleaned data so can read this in in future
+for i in subs:
+    sub   = dict(loc = 'workstation', id = i)
+    param = get_subject_info_wmConfidence(sub)
+    
+    if i in [1,2]: #these are two pilot subjects that have 10 blocks in one session, probably not going to be used
+        raw = mne.io.read_raw_eeglab(
+                input_fname=param['rawset'],
+                montage = 'easycap-M1',
+                eog=['VEOG', 'HEOG'], preload = True)
+        raw.rename_channels({'PO6':'PO8'})
+        raw.set_montage('easycap-M1')
+        
+        raw.filter(1, 40) #filter raw data
+        ica = mne.preprocessing.ICA(n_components = .99, method = 'infomax').fit(raw)
+        eog_epochs = mne.preprocessing.create_eog_epochs(raw)
+        eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, threshold=2)
+        ica.plot_scores(eog_scores)
+        
+        ica.plot_components(inst=raw)
+        ica.exclude.extend(eog_inds)
+        ica.apply(inst=raw)
+        
+        raw.save(fname = param['rawcleaned'], fmt='double')
+    elif i == 3:
+        raw = mne.io.read_raw_eeglab(
+                input_fname=param['rawset_sess1'],
+                montage = 'easycap-M1',
+                eog=['VEOG', 'HEOG'], preload = True)
+        raw.set_montage('easycap-M1')
+        
+        raw.filter(1, 40) #filter raw data
+        ica = mne.preprocessing.ICA(n_components = .99, method = 'infomax').fit(raw)
+        eog_epochs = mne.preprocessing.create_eog_epochs(raw)
+        eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, threshold=2)
+        ica.plot_scores(eog_scores)
+        
+        ica.plot_components(inst=raw)
+        ica.exclude.extend(eog_inds)
+        ica.apply(inst=raw)
+        
+        raw.save(fname = param['rawcleaned_sess1'], fmt='double')
+        
+    else:
+        for part in [1, 2]:
+            raw = mne.io.read_raw_eeglab(
+                input_fname=param['rawset_sess'+str(part)],
+                montage = 'easycap-M1',
+                eog=['VEOG', 'HEOG'], preload = True)
+            raw.set_montage('easycap-M1')
+            
+            raw.filter(1, 40) #filter raw data
+            ica = mne.preprocessing.ICA(n_components = .99, method = 'infomax').fit(raw)
+            eog_epochs = mne.preprocessing.create_eog_epochs(raw)
+            eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, threshold=1)
+            eog_inds
+            #ica.plot_scores(eog_scores)
+            
+            ica.plot_components(inst=raw)
+            ica.exclude.extend(eog_inds[0:2])
+            ica.apply(inst=raw)
+            
+            raw.save(fname = param['rawcleaned_sess'+str(part)], fmt='double')
 
