42e5bb3d56519241c869b1b2b4e85dc48b354259
Sammi Chekroud <sammi.r.chekroud@gmail.com>
2019-07-01 11:00:10 +0100

    update matlab function that gets subject paths and params

--- analysis_scripts/toolbox/getSubjectInfo_wmConf.m
+++ analysis_scripts/toolbox/getSubjectInfo_wmConf.m
@@ -2,37 +2,35 @@ function param = getSubjectInfo_wmConf(subj)
 
 param.path = '/home/sammirc/Desktop/DPhil/wmConfidence/data';
 
-if subj == 1;
-    param.subid                     = 's01';
-    param.behaviour                 = '/datafiles/s01/wmConfidence_eegfmri_outsidescanner_S01_allData_preprocessed.csv';
-    param.rawdata                   = '/eeg/s01/wmConfidence_s01_12062019.cdt';
-    param.rawset                    = '/eeg/s01/wmConfidence_s01_12062019.set';
-    param.cuelock_noref             = '/eeg/s01/wmConfidence_s01_cuelock_noref.mat';
-    param.cuelock_car               = '/eeg/s01/wmConfidence_s01_cuelock_common.mat';
-    param.cuelock_mast              = '/eeg/s01/wmConfidence_s01_cuelock_mast.mat';
-    param.cuelock_noref_laplacian   = '/eeg/s01/wmConfidence_s01_cuelock_noref_laplacian.mat';
-    param.cuelock_car_laplacian     = '/eeg/s01/wmConfidence_s01_cuelock_common_laplacian.mat';
-    param.cuelock_mast_laplacian    = '/eeg/s01/wmConfidence_s01_cuelock_mast_laplacian.mat';
-    param.includechans = {'FP1','FPZ','FP2','AF3','AF4','F7','F5','F3','F1','FZ','F2','F4','F6','F8','FC5','FC3','FC1','FCZ','FC2','FC4','FC6','T7','C5','C3','C1','CZ', 'C4','C6','T8','TP7', ... %C2 excluded broken channel
-                          'CP5','CP3','CP1','CPZ', 'CP2','CP4','CP6','TP8','M1','M2','P7','P5','P3','P1', 'PZ', 'P2','P4','P6','P8','PO7','PO3','POZ','PO4','PO8','O1','OZ','O2','VEO','HEO'}; 
-    param.car_chans = {'FP1','FPZ','FP2','AF3','AF4','F7','F5','F3','F1','FZ','F2','F4','F6','F8','FC5','FC3','FC1','FCZ','FC2','FC4','FC6','T7','C5','C3','C1','CZ', 'C4','C6', ... %C2 excluded broken channel
-                          'T8','TP7','CP5','CP3','CP1','CPZ', 'CP2','CP4','CP6','TP8','P7','P5','P3','P1', 'PZ', 'P2','P4','P6','P8','PO7','PO3','POZ','PO4','PO8','O1','OZ','O2'}; 
+if subj == 1 ;
+    param.rawdata = '/eeg/s01/wmConfidence_s01_12062019.cdt';
+    param.rawset  = '/eeg/s01/wmConfidence_s01_12062019.set';
 elseif subj == 2;
-    param.subid                     = 's02';
-    param.behaviour                 = '/datafiles/s02/wmConfidence_eegfmri_outsidescanner_S02_allData_preprocessed.csv';
-    param.rawdata                   = '/eeg/s02/wmConfidence_s02_12062019.cdt';
-    param.rawset                    = '/eeg/s02/wmConfidence_s02_12062019.set';
-    param.cuelock_noref             = '/eeg/s02/wmConfidence_s02_cuelock_noref.mat';
-    param.cuelock_car               = '/eeg/s02/wmConfidence_s02_cuelock_common.mat';
-    param.cuelock_mast              = '/eeg/s02/wmConfidence_s02_cuelock_mast.mat';
-    param.cuelock_noref_laplacian   = '/eeg/s02/wmConfidence_s02_cuelock_noref_laplacian.mat';
-    param.cuelock_car_laplacian     = '/eeg/s02/wmConfidence_s02_cuelock_common_laplacian.mat';
-    param.cuelock_mast_laplacian    = '/eeg/s02/wmConfidence_s02_cuelock_mast_laplacian.mat';
-    param.includechans = {'FP1','FPZ','FP2','AF3','AF4','F7','F5','F3','F1','FZ','F2','F4','F6','F8','FC5','FC3','FC1','FCZ','FC2','FC4','FC6','T7','C5','C3','C1','CZ', 'C4','C6','T8','TP7', ... %C2 excluded broken channel
-                          'CP5','CP3','CP1','CPZ', 'CP2','CP4','CP6','TP8','M1','M2','P7','P5','P3','P1', 'PZ', 'P2','P4','P6','P8','PO7','PO3','POZ','PO4','PO8','O1','OZ','O2','VEO','HEO'}; 
-    param.car_chans = {'FP1','FPZ','FP2','AF3','AF4','F7','F5','F3','F1','FZ','F2','F4','F6','F8','FC5','FC3','FC1','FCZ','FC2','FC4','FC6','T7','C5','C3','C1','CZ', 'C4','C6', ... %C2 excluded broken channel
-                          'T8','TP7','CP5','CP3','CP1','CPZ', 'CP2','CP4','CP6','TP8','P7','P5','P3','P1', 'PZ', 'P2','P4','P6','P8','PO7','PO3','POZ','PO4','PO8','O1','OZ','O2'}; 
-    
-
+    param.rawdata = '/eeg/s02/wmConfidence_s02_12062019.cdt';
+    param.rawset  = '/eeg/s02/wmConfidence_s02_12062019.set';
+elseif subj == 3;
+    param.rawdata = '/eeg/s03/wmConfidence_s03a_24062019.cdt';
+    param.rawset  = '/eeg/s03/wmConfidence_s03a_24062019.set';
+elseif subj == 4;
+    param.rawdata = '/eeg/s04/wmConfidence_s04a_24062019.cdt';
+    param.rawset  = '/eeg/s04/wmConfidence_s04a_24062019.set';
+    param.rawdata2 = '/eeg/s04/wmConfidence_s04b_24062019.cdt';
+    param.rawset2  = '/eeg/s04/wmConfidence_s04b_24062019.set';
+elseif subj == 5;
+    param.rawdata = '/eeg/s05/wmConfidence_s05a_25062019.cdt';
+    param.rawset  = '/eeg/s05/wmConfidence_s05a_25062019.set';
+    param.rawdata2 = '/eeg/s05/wmConfidence_s05b_25062019.cdt';
+    param.rawset2  = '/eeg/s05/wmConfidence_s05b_25062019.set';
+elseif subj == 6;
+    param.rawdata = '/eeg/s06/wmConfidence_s06a_26062019.cdt';
+    param.rawset  = '/eeg/s06/wmConfidence_s06a_26062019.set';
+    param.rawdata2 = '/eeg/s06/wmConfidence_s06b_26062019.cdt';
+    param.rawset2  = '/eeg/s06/wmConfidence_s06b_26062019.set';
+elseif subj == 7;
+    param.rawdata = '/eeg/s07/wmConfidence_s07a_26062019.cdt';
+    param.rawset  = '/eeg/s07/wmConfidence_s07a_26062019.set';
+    param.rawdata2 = '/eeg/s07/wmConfidence_s07b_26062019.cdt';
+    param.rawset2  = '/eeg/s07/wmConfidence_s07b_26062019.set';
+ 
 end
 end
