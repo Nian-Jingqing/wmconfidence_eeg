a25e44325930169c618a60e2f617b8b1531e6f22
Sammi Chekroud <sammi.r.chekroud@gmail.com>
2019-07-01 11:00:35 +0100

    bump script that loops over subjects to convert cdt data to set data for mne io

--- analysis_scripts/matlab/wmConfidence_ConvertEEG_toSet.m
+++ analysis_scripts/matlab/wmConfidence_ConvertEEG_toSet.m
@@ -13,15 +13,19 @@ addpath(genpath(eeglabpath));
 addpath([ '/home/sammirc/Desktop/DPhil/wmConfidence/analysis_scripts/toolbox' ]);
 %%
 
-subs = [1 2];
+subs = [1 2 3 4 5 6 7];
 
 
 for i = 1:length(subs)
     sub = subs(i);
-    
     param = getSubjectInfo_wmConf(sub);
-    
+    %convert first session of data
     d = pop_loadcurry([param.path, param.rawdata]);
     pop_saveset(d, 'filename', param.rawset, 'filepath', param.path);
     clear d;
+    if sub > 3; %most subjects have second session of data, so convert these too
+        d = pop_loadcurry([param.path, param.rawdata2]);
+        pop_saveset(d, 'filename', param.rawset2, 'filepath', param.path)
+        clear d;
+    end
 end
