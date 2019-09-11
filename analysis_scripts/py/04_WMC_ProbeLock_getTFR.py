b9f2539a4e1c186ccb6b2c25c00fd8aacd905813
Sammi Chekroud <sammi.r.chekroud@gmail.com>
2019-09-11 16:09:58 +0100

    scripts to do TF decomposition of the epoched data

--- analysis_scripts/py/04_WMC_ProbeLock_getTFR.py
+++ analysis_scripts/py/04_WMC_ProbeLock_getTFR.py
@@ -24,18 +24,18 @@ wd = '/home/sammirc/Desktop/DPhil/wmConfidence' #workstation wd
 os.chdir(wd)
 
 
-sublist = np.array([1,2,3,4,5,6,7,8,9,10])
-
-for i in sublist:
+subs = np.array([1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+subs = np.array([11,12,13,14,15])
+subs = np.array([11, 15])
+for i in subs:
     print('\n\nworking on subject ' + str(i) +'\n\n')
     sub = dict(loc = 'workstation', id = i)
     param = get_subject_info_wmConfidence(sub)
-    
+
     probelocked = mne.epochs.read_epochs(fname = param['probelocked'], preload=True) #read raw data
-    probelocked.resample(250) #downsample to 250hz so don't overwork the workstation lols
-    if i == 10: #not sure why, need to check, but metadata not in this datafile...
-        probelocked.metadata = pd.DataFrame.from_csv(path = param['behaviour_blinkchecked'])
-    
+    probelocked.resample(100) #downsample to 250Hz so don't overwork the workstation (100hz should be fine for frequencies up to 50Hz in brain)
+
+
     #will do an automated process of looking for trials with heightened variance (noise) and output which trials to keep
     _, keeps = plot_AR(probelocked, method = 'gesd', zthreshold = 1.5, p_out=.1, alpha = .05, outlier_side = 1)
     keeps = keeps.flatten()
@@ -43,21 +43,21 @@ for i in sublist:
     discards = np.ones(len(probelocked), dtype = 'bool')
     discards[keeps] = False
     probelocked = probelocked.drop(discards) #first we'll drop trials with excessive noise in the EEG
-    
+
     #now we'll drop trials with behaviour problems (reaction time +/- 2.5 SDs of mean, didn't click to report orientation)
     probelocked = probelocked['DTcheck == 0 and clickresp == 1 and arraycueblink == 0'] #also exclude trials where blinks happened in the array or cue period
     probelocked.set_eeg_reference(ref_channels=['RM']) #re-reference to average of the two mastoids
-    
+
     # set up params for TF decomp
     freqs = np.arange(1, 40, 1)  # frequencies from 2-35Hz
     n_cycles = freqs *.3  # 300ms timewindow for estimation
-    
+
     print('\nrunning TF decomposition\n')
     # Run TF decomposition overall epochs
     tfr = mne.time_frequency.tfr_morlet(probelocked, freqs=freqs, n_cycles=n_cycles,
                          use_fft=True, return_itc=False, average=False)
     tfr.metadata.to_csv(param['probelocked_tfr_meta'], index=False)
     tfr.save(fname = param['probelocked_tfr'], overwrite = True)
-    
+
     del(probelocked)
     del(tfr)
--- analysis_scripts/py/04_WMC_ResponseLock_getTFR.py
+++ analysis_scripts/py/04_WMC_ResponseLock_getTFR.py
@@ -24,17 +24,18 @@ wd = '/home/sammirc/Desktop/DPhil/wmConfidence' #workstation wd
 os.chdir(wd)
 
 
-sublist = np.array([1,2,3,4,5,6,7,8,9,10])
-
-for i in sublist:
+subs = np.array([1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+subs = np.array([11,12,13,14,15])
+subs = np.array([11, 15])
+for i in subs:
     print('\n\nworking on subject ' + str(i) +'\n\n')
     sub = dict(loc = 'workstation', id = i)
     param = get_subject_info_wmConfidence(sub)
-    
+
     resplocked = mne.epochs.read_epochs(fname = param['resplocked'], preload=True) #read raw data
     resplocked.resample(250) #downsample to 250hz so don't overwork the workstation lols
 
-    
+
     #will do an automated process of looking for trials with heightened variance (noise) and output which trials to keep
     _, keeps = plot_AR(resplocked, method = 'gesd', zthreshold = 1.5, p_out=.1, alpha = .05, outlier_side = 1)
     keeps = keeps.flatten()
@@ -42,21 +43,21 @@ for i in sublist:
     discards = np.ones(len(resplocked), dtype = 'bool')
     discards[keeps] = False
     resplocked = resplocked.drop(discards) #first we'll drop trials with excessive noise in the EEG
-    
+
     #now we'll drop trials with behaviour problems (reaction time +/- 2.5 SDs of mean, didn't click to report orientation)
     resplocked = resplocked['DTcheck == 0 and clickresp == 1 and arraycueblink == 0'] #also exclude trials where blinks happened in the array or cue period
     resplocked.set_eeg_reference(ref_channels=['RM']) #re-reference to average of the two mastoids
-    
+
     # set up params for TF decomp
     freqs = np.arange(1, 40, 1)  # frequencies from 2-35Hz
     n_cycles = freqs *.3  # 300ms timewindow for estimation
-    
+
     print('\nrunning TF decomposition\n')
     # Run TF decomposition overall epochs
     tfr = mne.time_frequency.tfr_morlet(resplocked, freqs=freqs, n_cycles=n_cycles,
                          use_fft=True, return_itc=False, average=False)
     tfr.metadata.to_csv(param['resplocked_tfr_meta'], index=False)
     tfr.save(fname = param['resplocked_tfr'], overwrite = True)
-    
+
     del(resplocked)
     del(tfr)
