09cce78f367be18fa976aefc40586d0acf22ad44
Sammi Chekroud <schekrou@fmrib.ox.ac.uk>
2019-10-16 17:37:54 +0100

    update script to interpolate missing samples at the beginning of a block where it prevents blink interpolation

--- analysis_scripts/py/wmc_eyetracking_preproc_sessions.py
+++ analysis_scripts/py/wmc_eyetracking_preproc_sessions.py
@@ -31,17 +31,18 @@ os.chdir(wd)
 
 
 subs = np.array([4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16])
+subs = np.array([   5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16])
 #%%
 for i in subs:
     for part in [1, 2]: #2 blocks per participant
         sub = dict(loc = 'laptop', id = i)
         param = get_subject_info_wmConfidence(sub)
         print('-- working on subject %d --\n'%(i))
-    
-    
-    
+
+
+
         print('-- working on part %d --\n\n'%(part))
-        
+
         data = bce.parse_eye_data(eye_fname = param['raweyes_sess'+str(part)], block_rec = True, trial_rec = False, nblocks = 8)
 
         nblocks = len(data)
@@ -49,100 +50,80 @@ for i in subs:
             raise bce.DataError('found %d blocks when expecting 8 in this block -- something went wrong with this recording'%nblocks)
 
         dat = deepcopy(data)
-        
+
         data = bce.nanblinkperiods_fromeyelink(data = data,
                                                nblocks = nblocks,
                                                traces_to_scan = ['lx', 'ly', 'lp', 'rx', 'ry', 'rp'],
                                                remove_win = 60) #50ms sample to remove around the start and end of the blink period
         #it seems like this routine is actually not too bad? if you use the blinks from the appropriate eyes to nan the blink period
         #it works ok using the eyelink blink identified areas
-        
+
         #this should just find the nanperiods that we made earlier
         data = bce.find_missing_periods(data = data,
                                         nblocks = nblocks,
                                         traces_to_scan = ['lp', 'rp', 'lx', 'rx', 'ly', 'ry'])
-        
+
         #if after running this line you run:
         for iblock in range(len(data)):
-            print(data[iblock]['Eblk_lx'][0])
-            print(data[iblock]['Eblk_rx'][0], '\n')
+            if i == 11 and part == 1 and iblock == 7: # this subject is missing data for one entire block in this session
+                print('skipping block %d for subject 11 session 1'%(iblock+1))
+                continue
+            if i == 11 and part == 2 and iblock in [2]: #eyes missing in this block
+                print('skipping block %d for subject 11 session 2'%(iblock+1))
+                continue
+            if i == 12 and part == 1 and iblock in [6]:
+                print('skipping block %d for subject 12 session 1'%(iblock+1))
+                continue
+            if i == 12 and part == 2 and iblock in [5,6]:
+                print('skipping block %d for subject 12 session 1'%(iblock+1))
+                continue
+            else:
+                print(data[iblock]['Eblk_lx'][0])
+                print(data[iblock]['Eblk_rx'][0], '\n')
+
         #this prints out the first blink in the data (period of missing data). if durations (last column) are negative
         #then there are nans at the beginning of the trace that need handling
-        
-#        if i == 4 and part == 1:
-#            #there is one block for this subject where the very first sample is a nan in both the left and right eye after interpolating blinks
-#            #the eyes must have just fallen out
-#            #this causes two problems:
-#            # 1 -- you get a blink offset but not onset (from the start) so all subsequent blink durations are negative
-#            #      and interpolation fails on the right eye
-#            # 2 -- prevents you from lpfiltering because it nans the entire trace when you filter
-#            
-#            #FIX: set specific values to the average of the first 10 samples in both left and right eyes
-#            #     NB this needs to be done prior to find_missing_periods or the blink (nan period) durations are all messed up
-#            #     not all problems need fixing pre-interpolation as some are related to non-blink missing data that can be interpolated elsewhere
-#            data[4]['lx'][0] = np.round(np.nanmean(data[4]['lx'][0:10]), 1) #round to 1 dp so same as other values
-#            data[4]['rx'][0] = np.round(np.nanmean(data[4]['rx'][0:10]), 1)
-#        if i  == 5 and part == 1:
-#            #in this session for subject 5, there are some blocks where there are nans at the start
-#            #this is evident after running bce.find_missing_periods if the duration of blinks (e.g. in data[4]['Eblk_lx']) are negative (last column)
-#            #for example, block 4 has two missing samples in the left eye at the beginning of the trace
-#            #we will set these values to the average of the first ten samples
-#            data[4]['lx'][0:2] = np.round(np.nanmean(data[4]['lx'][0:10]), 1)      
-#            data[4]['rx'][0:3] = np.round(np.nanmean(data[4]['rx'][0:10]), 1) #in the right eye its the first three samples that are missing
-#            
-#            #block 5 has the same problem
-#            data[5]['lx'][0:2] = np.round(np.nanmean(data[5]['lx'][0:10]), 1) #in the left eye in this block its the first two samples
-#            data[5]['rx'][0:5] = np.round(np.nanmean(data[5]['rx'][0:10]), 1) #in the right eye its the first four samples
-#            
-#            #block 6 is a longer starting blink (it just means the blink started before the recording started really...)
-#            data[6]['lx'][0:238] = np.round(np.nanmean(data[6]['lx'][0:250]), 1) #so we'll give it a slightly longer period to get data
-#            data[6]['rx'][0:374] = np.round(np.nanmean(data[6]['rx'][0:400]), 1) #even longer period at the start here so get average for a bit longer
-#            
-#            #block 7 is a shorter messed up period
-#            data[7]['lx'][0:4]   = np.round(np.nanmean(data[7]['lx'][0:10]), 1) #so give it the average of the first 10 samples here
-#            data[7]['rx'][0:4]   = np.round(np.nanmean(data[7]['rx'][0:10]), 1) #so give it the average of the first 10 samples here
-#        if i == 5 and part == 2:
-#            #some blocks also have the same problem here: #blocks = [1, 4, 7]
-#            data[1]['lx'][0:128] = np.round(np.nanmean(data[1]['lx'][0:150]), 1)
-#            data[1]['rx'][0:110] = np.round(np.nanmean(data[1]['rx'][0:120]), 1)
-#            
-#            #4th block is missing fewer data points at the beginning of the right x trace
-#            data[4]['lx'][:94] = np.round(np.nanmean(data[4]['lx'][:100]), 1)
-#            data[4]['rx'][0]   = np.round(np.nanmean(data[4]['rx'][0:10]), 1)
-#            
-#            data[7]['lx'][:245] = np.round(np.nanmean(data[7]['lx'][:260]), 1)
-#            data[7]['rx'][:213] = np.round(np.nanmean(data[7]['rx'][:220]), 1)
-#        if i == 7 and part == 1:
-#            data[2]['rx'][:505] = np.round(np.nanmean(data[2]['rx'][:510]), 1)
-#        if i == 8 and part == 1:
-#            #blocks = [1, 2, 6]
-#            data[1]['lx'][:104] = np.round(np.nanmean(data[1]['lx'][:110]), 1)
-#            data[2]['lx'][:2]   = np.round(np.nanmean(data[2]['lx'][:10]),  1)
-#            data[2]['rx'][:4]   = np.round(np.nanmean(data[2]['rx'][:10]),  1)
-#            data[6]['lx'][:2]   = np.round(np.nanmean(data[6]['lx'][:10]),  1)
-#            data[6]['rx'][:3]   = np.round(np.nanmean(data[6]['rx'][:10]),  1)
-#        if i == 8 and part == 2: #blocks = [2,5]
-#            data[2]['lx'][:140] = np.round(np.nanmean(data[2]['lx'][:150]), 1)
-#            data[2]['rx'][:10]  = np.round(np.nanmean(data[2]['rx'][:15]),  1)
-#            data[5]['lx'][0]    = np.round(np.nanmean(data[5]['lx'][:10]),  1)
-#            data[5]['rx'][:2]   = np.round(np.nanmean(data[5]['rx'][:10]),  1)
-#        if i == 9 and part == 2: #blocks = [6,7]
-        
 
         #finds if the block was started with a blink before recording began
         #extrapolates first 10 samples to the beginning of the trace
-        data = bce.check_nansatstart(data = data)  
+        if i == 11 and part == 1:
+            data = bce.check_nansatstart(data = data,
+                                         blocks_to_ignore = np.subtract([8], 1) ) #block 8 (last block) missing eyes)
+        elif i == 11 and part == 2:
+            data = bce.check_nansatstart(data = data,
+                                         blocks_to_ignore = np.subtract([3], 1) ) #block 3 missing eyes
+        elif i == 12 and part == 1:
+            data = bce.check_nansatstart(data = data,
+                                         blocks_to_ignore = np.subtract([6], 1) ) #block 3 missing eyes
+        elif i == 12 and part == 2:
+            data = bce.check_nansatstart(data = data,
+                                         blocks_to_ignore = np.subtract([6, 7], 1))
+        else:
+            data = bce.check_nansatstart(data = data)
         #this should just find the nanperiods that we made earlier
         #rerunning this is essential to check if the above function has worked
         data = bce.find_missing_periods(data = data,
                                         nblocks = nblocks,
                                         traces_to_scan = ['lp', 'rp', 'lx', 'rx', 'ly', 'ry'])
-        
+
         #if after running this line you run:
         for iblock in range(len(data)):
-            print(data[iblock]['Eblk_lx'][0])
-            print(data[iblock]['Eblk_rx'][0], '\n')
-            #this should now output no negative duration blinks if it's worked properly            
+            if i == 11 and part == 1 and iblock == 7: # this subject is missing data for one entire block in this session
+                print('skipping block %d for subject 11 session 1'%(iblock+1))
+                continue
+            if i == 11 and part == 2 and iblock in [2]: #eyes missing in this block
+                print('skipping block %d for subject 11 session 2'%(iblock+1))
+                continue
+            if i == 12 and part == 1 and iblock in [6]:
+                print('skipping block %d for subject 12 session 1'%(iblock+1))
+                continue
+            if i == 12 and part == 2 and iblock in [5,6]:
+                print('skipping block %d for subject 12 session 1'%(iblock+1))
+                continue
+            else:
+                print(data[iblock]['Eblk_lx'][0])
+                print(data[iblock]['Eblk_rx'][0], '\n')
+            #this should now output no negative duration blinks if it's worked properly
         #this prints out the first blink in th left eye trace. if the last column is negative, you have a problem at the beginning of the trace
 
         blockid = 1
@@ -152,13 +133,14 @@ for i in subs:
             for trace in clean_traces:
                 block = bce.interpolateBlinks_Blocked(block, trace, method = 'linear')
             blockid += 1
-            
-        if i == 5 and part == 1:
+
+        plotsubs_blocks = True
+        if plotsubs_blocks:
             for iblock in range(len(data)):
                 fig = plt.figure()
                 ax1 = fig.add_subplot(211)
                 ax2 = fig.add_subplot(212)
-                fig.suptitle('block %d'%iblock)
+                fig.suptitle('block %d'%(iblock+1))
                 ax1.plot(data[iblock]['rx'])
                 remsampsr = data[iblock]['rblink_removedsamps']
                 nansampsr = np.squeeze(np.where(np.isnan(data[iblock]['rx'])))
@@ -167,7 +149,7 @@ for i in subs:
                 if nansampsr.size > 0:
                     ynansampsr = np.multiply(np.ones(nansampsr.size), 780)
                     ax1.scatter(nansampsr, ynansampsr, color = 'green', marker = '.')
-                
+
                 ax2.plot(data[iblock]['lx'])
                 remsampsl = data[iblock]['lblink_removedsamps']
                 nansampsl = np.squeeze(np.where(np.isnan(data[iblock]['lx'])))
@@ -188,6 +170,6 @@ for i in subs:
 #        ax.set_title('right x')
 #        ax.plot(dat[iblock]['rx'], lw = .75)
 #        ax.plot(data[iblock]['rx'], lw = .75)
-        
+
         with open(param['cleanedeyes_sess'+str(part)], 'wb') as handle:
             pickle.dump(data, handle)
