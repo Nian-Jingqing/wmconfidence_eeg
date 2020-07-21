a5885a3e4a7fe3a128478ce1181f8b2afcc0deff
Sammi Chekroud <sammi.r.chekroud@gmail.com>
2020-07-21 20:42:20 +0100

    bump script

--- analysis_scripts/pyscripts/03_WMC_CueLock_CombineSessions.py
+++ analysis_scripts/pyscripts/03_WMC_CueLock_CombineSessions.py
@@ -19,11 +19,12 @@ from matplotlib import pyplot as plt
 #sys.path.insert(0, '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence_eegfmri/analysis_scripts')
 sys.path.insert(0, '/home/sammirc/Desktop/DPhil/wmConfidence/analysis_scripts')
 from wmConfidence_funcs import get_subject_info_wmConfidence
+from wmConfidence_funcs import gesd, plot_AR
+
 
 wd = '/Users/sammi/Desktop/Experiments/DPhil/wmConfidence'; #laptop wd
 wd = '/home/sammirc/Desktop/DPhil/wmConfidence' #workstation wd
 os.chdir(wd)
-
 
 subs = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26])
 subs = np.array([         4, 5, 6, 7, 8, 9,     11, 12, 13, 14, 15, 16, 17, 18,     20, 21, 22,     24, 25, 26])
@@ -122,8 +123,8 @@ for i in subs:
         raw1 = mne.io.read_raw_fif(fname = param['rawcleaned_sess1'], preload=True)
         raw2 = mne.io.read_raw_fif(fname = param['rawcleaned_sess2'], preload=True)
 
-        raw1.set_montage('easycap-M1');
-        raw2.set_montage('easycap-M1')
+#        raw1.set_montage('easycap-M1');
+#        raw2.set_montage('easycap-M1')
 
         #epoching
         #here it's important to specify a dictionary that assigns each trigger to its own integer value
@@ -154,12 +155,48 @@ for i in subs:
         cuelocked2   = mne.Epochs(raw2, events2, events_cue, tmin, tmax, baseline, reject_by_annotation=False, preload=True)
 
         bdata1 = pd.read_csv(param['behaviour_blinkchecked1'], index_col=None)
+        bdata1['prevtrlconfdiff'] = bdata1.confdiff.shift(1)  #write down on each trial what the previous trial error awareness was
+        bdata1['nexttrlconfdiff'] = bdata1.confdiff.shift(-1) #write down on each trial what the next trials error awareness is (used in glm)
+        bdata1['prevtrlcw']       = bdata1.confwidth.shift(1) #what was the confidence width on the previous trial?
+        bdata1['nexttrlcw']       = bdata1.confwidth.shift(-1) #and what is the confidence width on the next trial?
+
         bdata2 = pd.read_csv(param['behaviour_blinkchecked2'], index_col=None)
+        bdata2['prevtrlconfdiff'] = bdata2.confdiff.shift(1)  #write down on each trial what the previous trial error awareness was
+        bdata2['nexttrlconfdiff'] = bdata2.confdiff.shift(-1) #write down on each trial what the next trials error awareness is (used in glm)
+        bdata2['prevtrlcw']       = bdata2.confwidth.shift(1) #what was the confidence width on the previous trial?
+        bdata2['nexttrlcw']       = bdata2.confwidth.shift(-1) #and what is the confidence width on the next trial?
         cuelocked1.metadata = bdata1
         cuelocked2.metadata = bdata2
 
-        cuelocked = mne.concatenate_epochs([cuelocked1, cuelocked2]) #combine the epoched data with aligned metadata
+        #bad channels get marked and cant concatenate epochs unless these are the same
+        if cuelocked1.info['bads'] != []:
+            cuelocked1.interpolate_bads(reset_bads = True)
+        if cuelocked2.info['bads'] != []:
+            cuelocked2.interpolate_bads(reset_bads = True)
 
+
+
+
+        cuelocked = mne.concatenate_epochs([cuelocked1, cuelocked2]) #combine the epoched data with aligned metadata
+        
+        #trial rejection here
+        #step1 - automated gesd removal
+        #step 2, just check these epochs to make sure some haven't slipped through the net by accident. looking for more catastrophic failures, or noise in baseline
+        _, keeps = plot_AR(cuelocked.pick_types(eeg=True), method = 'gesd', zthreshold = 1.5, p_out=.1, alpha = .05, outlier_side = 1)
+        keeps = keeps.flatten()
+        plt.close()
+
+        discards = np.ones(len(cuelocked), dtype = 'bool')
+        discards[keeps] = False
+        cuelocked = cuelocked.drop(discards) #first we'll drop trials with excessive noise in the EEG
+
+        cuelocked = cuelocked['DTcheck == 0 and clickresp == 1'] #the last trial of the session doesn't have a following trial!
+
+        #now go through manually
+#        cuelocked.plot(n_channels=62, scalings = dict(eeg=200e-6), n_epochs=3)
+        
+        
+        
         cuelocked.save(fname = param['cuelocked'], overwrite=True)
         del(cuelocked)
         del(cuelocked1)
